ca5b81dfcae30ab97ea1e4af339bdbadfe7394ff
mitchmarns <sakurapizzaa@gmail.com>
2025-03-19 15:22:50 -0700

    fixes

--- config/db/connection.js
+++ config/db/connection.js
@@ -1,5 +1,5 @@
 // connection.js - Fixed with absolute path handling
-const mysql = require('mysql2');
+const sqlite3 = require('mysql2').verbose();
 const path = require('path');
 const fs = require('fs');
 
@@ -19,19 +19,16 @@ if (!dbExists) {
 }
 
 // Create a database connection
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'username_hockey_app_user',  // Your created username
-  password: 'your_strong_password',  // Your created password
-  database: 'username_hockey_roleplay'  // Your created database name
-});
-
-db.connect((err) => {
+const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
+    console.error('Error connecting to database:', err);
+    process.exit(1);
   }
-  console.log('Connected to MySQL database');
+  
+  console.log('Successfully connected to database at:', dbPath);
+  
+  // Enable foreign keys
+  db.run('PRAGMA foreign_keys = ON');
 });
 
 module.exports = { db };
